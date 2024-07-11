f0ee5a33c74555c23fe4a244fa3bbfa829379b60
EliNygard <eli.nygard@gmail.com>
2024-07-11 13:41:26 +0200

    js toggle menu

--- javascript/ui/topNav.mjs
+++ javascript/ui/topNav.mjs
@@ -1,12 +1,15 @@
 const btnOpen = document.querySelector("#btnOpen");
 const btnClose = document.querySelector("#btnClose");
+const menu = document.querySelector(".topnav__menu")
 
 export function openMobileMenu() {
   btnOpen.setAttribute("aria-expanded", "true");
+  menu.classList.toggle("open", true)
 }
 
 export function closeMobileMenu() {
   btnClose.setAttribute("aria-expanded", "false");
+  menu.classList.toggle("open", false)
 }
 
 btnOpen.addEventListener("click", openMobileMenu);
