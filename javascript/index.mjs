2fb884defc4f8c28324cdb72dce60518fa39cf40
EliNygard <eli.nygard@gmail.com>
2024-09-04 18:06:07 +0200

    added new index file to handle all js

--- javascript/index.mjs
+++ javascript/index.mjs
@@ -1,1 +1,11 @@
+import NoroffAPI from "./api/index.mjs";
+import { currentPostId } from "./utilities/currentPostId.mjs";
 
+
+const api = new NoroffAPI("https://v2.api.noroff.dev")
+
+const post = await api.post.get("9d3df64e-b692-40c9-882e-9104193c511b")
+console.log(post);
+
+
+const postId = currentPostId();
